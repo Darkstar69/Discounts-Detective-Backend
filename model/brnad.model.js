66b2ad6156952d24ffe05787dafca047079a2554
sumit <sumitkanth999@gmail.com>
2024-07-11 14:14:08 +0530

    added brand schema

---
+++ model/brnad.model.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+const brandSchema = new mongoose.Schema({
+    name: {
+        type: String, 
+        required: true,
+        unique: true
+    },
+    icon: {
+        type: String
+    }
+}, {timestamps: true});
+
+export const Brand = mongoose.model('Brand', brandSchema);
